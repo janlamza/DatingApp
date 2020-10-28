8b71208a54a093a6d293b96417f91f2be0985e05
jan <janlamza12@gmail.com>
2020-10-28 16:10:52 +0100

    End of section 4

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string password { get; set; }
+    }
+}
