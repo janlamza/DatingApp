a2b446d351b0c636dcd4ee3f812954c7e01449cb
jan <janlamza12@gmail.com>
2020-11-10 23:17:21 +0100

    End of section 12

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -9,7 +9,8 @@ namespace API.DTOs
     {
         public string Username { get; set; }
         public string Token { get; set; }
-
+        
         public string PhotoUrl { get; set; }
+        public string KnownAs { get; set; }
     }
 }
