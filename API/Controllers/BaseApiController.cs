c485213a28ed204d1d2447705870a29717d9ceb5
jan <janlamza12@gmail.com>
2020-12-05 12:24:08 +0100

    Section 19 migrations commit

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-    [ServiceFilter(typeof(LogUserActivity))]
+    
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController: ControllerBase
